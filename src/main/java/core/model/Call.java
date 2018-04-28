90bfa1acc1b336fd0de2d288e6f264cf24e73dba
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-04-28 16:15:00 -0300

    Adding Call model

---
+++ src/main/java/core/model/Call.java
@@ -0,0 +1,40 @@
+package core.model;
+
+import java.util.UUID;
+
+/**
+ * 
+ * @author paulofelipe
+ *
+ */
+public class Call {
+	
+	private String id;
+	private Integer machineNumber;
+	private String lccUsername;
+	private Long createAt;
+
+	public Call(Integer machineNumber, String lccUsername) {
+		this.id = UUID.randomUUID().toString();
+		this.machineNumber = machineNumber;
+		this.lccUsername = lccUsername;
+		this.createAt = System.currentTimeMillis();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public Integer getMachineNumber() {
+		return machineNumber;
+	}
+
+	public String getLccUsername() {
+		return lccUsername;
+	}
+
+	public Long getCreateAt() {
+		return createAt;
+	}
+	
+}
