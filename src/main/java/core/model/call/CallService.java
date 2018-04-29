a119621e5d2487c65887cabca8612a680fddb2dc
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-04-29 15:58:15 -0300

    Implementing CallService

--- src/main/java/core/model/call/CallService.java
+++ src/main/java/core/model/call/CallService.java
@@ -1,5 +1,24 @@
 package core.model.call;
 
-public interface CallService {
+import java.util.Collection;
 
+import core.model.PCMException;
+
+/**
+ * 
+ * @author pfelipe
+ *
+ */
+public interface CallService {
+	
+	public static final String CALL_ALREADY_EXIST = "Call already exist";
+	public static final String CALL_DOES_NOT_EXIST = "There is no call";
+
+	public void addCall(Call call) throws PCMException;
+	
+	public Collection<Call> getAllCalls();
+	
+	public void deleteCall(String callId) throws PCMException;
+	
+	public void deleteAllCalls() throws PCMException;
 }
--- src/main/java/core/model/call/DefaultCallService.java
+++ src/main/java/core/model/call/DefaultCallService.java
@@ -1,4 +1,53 @@
 package core.model.call;
 
+import java.util.Collection;
+import java.util.PriorityQueue;
+
+import core.model.PCMException;
+
+/**
+ * 
+ * @author pfelipe
+ *
+ */
 public class DefaultCallService implements CallService {
+	Collection<Call> callQueue;
+
+	public DefaultCallService() {
+		this.callQueue = new PriorityQueue<Call>();
+	}
+
+	@Override
+	public Collection<Call> getAllCalls() {
+		return this.callQueue;
+	}
+
+	@Override
+	public void deleteCall(String callId) throws PCMException {
+		Call callToBeDeleted = null;
+		for (Call call : this.callQueue) {
+			String queueCallId = call.getId();
+			if (queueCallId.equals(callId)) {
+				callToBeDeleted = call;
+				break;
+			}
+		}
+		if (callToBeDeleted == null) {
+			throw new PCMException(CallService.CALL_DOES_NOT_EXIST + " with id [" + callId + "]");
+		}
+		this.callQueue.remove(callToBeDeleted);
+	}
+
+	@Override
+	public void deleteAllCalls() throws PCMException {
+		this.callQueue = new PriorityQueue<Call>();
+	}
+
+	@Override
+	public void addCall(Call call) throws PCMException {
+		if (this.callQueue.contains(call)) {
+			throw new PCMException(CallService.CALL_ALREADY_EXIST);
+		}
+		this.callQueue.add(call);
+	}
 }
