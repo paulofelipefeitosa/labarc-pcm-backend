0d51ad3105ca46874fe2c676ac2e694bef9f34b2
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-04-29 16:41:00 -0300

    Implementing isValid, hashCode and equals Token method

--- src/main/java/authentication/token/Token.java
+++ src/main/java/authentication/token/Token.java
@@ -21,8 +21,33 @@ public class Token {
 		return id;
 	}
 
-	public Long getValidity() {
-		return validity;
+	public boolean isValid() {
+		return this.validity > System.currentTimeMillis();
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
