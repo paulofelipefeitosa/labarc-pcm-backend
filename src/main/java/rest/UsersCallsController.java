ea8e2a1968e7fe2c77d5f464f0e6754c88a45d79
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-04-28 22:51:05 -0300

    Adding RESTful API with Spring Boot

---
+++ src/main/java/rest/UsersCallsController.java
@@ -0,0 +1,22 @@
+package rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import core.model.call.Call;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "users/calls")
+public class UsersCallsController {
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<String> createCompute(@RequestBody Call computeOrder) {
+		return new ResponseEntity<String>(HttpStatus.CREATED);
+	}
+}
