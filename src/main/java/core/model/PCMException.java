011a8ef734919a6c8c841ea3eb6fe6986c445db2
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-04-29 15:59:05 -0300

    Implementing PCMException

---
+++ src/main/java/core/model/PCMException.java
@@ -0,0 +1,14 @@
+package core.model;
+
+public class PCMException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PCMException(String message) {
+		super(message);
+	}
+
+}
