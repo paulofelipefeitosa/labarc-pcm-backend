69b7f2eb394bcc299d7f1b1e1b09abcf30bb6860
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-05-07 13:50:10 -0300

    Removing unused constant

--- src/main/java/pcm/LabarcPCMApplication.java
+++ src/main/java/pcm/LabarcPCMApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabarcPCMApplication {
 	
-	public static final String PCM_CONF_FILE_NOT_FOUND_MSG = "There is no pcm configuration file";
-
 	public static void main(String[] args) {
 		SpringApplication.run(LabarcPCMApplication.class, args);
 	}
