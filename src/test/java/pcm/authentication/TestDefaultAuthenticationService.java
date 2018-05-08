c42310a0aeaf964e2c6775788794fa622e59168e
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-05-07 22:16:56 -0300

    Updating DefaultAuthenticationService tests

--- src/test/java/pcm/authentication/TestDefaultAuthenticationService.java
+++ src/test/java/pcm/authentication/TestDefaultAuthenticationService.java
@@ -1,92 +1,74 @@
 package pcm.authentication;
 
-import java.util.Properties;
-
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import pcm.PCMProperties;
 import pcm.authentication.DefaultAuthenticationService;
 import pcm.authentication.model.Token;
 import pcm.core.exception.PCMException;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestDefaultAuthenticationService {
-/**
-	private Properties properties;
-	
+
+	@Autowired
 	private DefaultAuthenticationService authenticationService;
-	
-	private final String username = "fake-admin";
-	private final String password = "fake-password";
-	
-	@Before
-	public void setUp() throws PCMException {
-		this.properties = new Properties();
-		this.properties.put(DefaultAuthenticationService.ADMIN_USERNAME_KEY, this.username);
-		this.properties.put(DefaultAuthenticationService.ADMIN_PASSWORD_KEY, this.password);
-		
-		this.authenticationService = new DefaultAuthenticationService(this.properties);
-	}
-	
-	@Test(expected = PCMException.class)
-	public void createAuthenticationServiceWithoutAdminUsername() throws PCMException {
-		Properties properties = new Properties();
-		properties.put(DefaultAuthenticationService.ADMIN_PASSWORD_KEY, this.password);
-		
-		new DefaultAuthenticationService(properties);
-	}
-	
-	@Test(expected = PCMException.class)
-	public void createAuthenticationServiceWithoutAdminPassword() throws PCMException {
-		Properties properties = new Properties();
-		properties.put(DefaultAuthenticationService.ADMIN_USERNAME_KEY, this.username);
-		
-		new DefaultAuthenticationService(properties);
-	}
-	
+
+	@Autowired
+	private PCMProperties properties;
+
 	@Test
 	public void testCreateValidToken() throws PCMException {
-		String acessId = this.authenticationService.createToken(this.username, this.password);
-		
+		String acessId = this.authenticationService.createToken(this.properties.getAdminUsername(),
+				this.properties.getAdminPassword());
+
 		Assert.assertNotNull(acessId);
 		Assert.assertFalse(acessId.trim().isEmpty());
 	}
-	
+
 	@Test(expected = PCMException.class)
 	public void testCreateInvalidTokenByUsername() throws PCMException {
-		this.authenticationService.createToken("invalid-username", this.password);
+		this.authenticationService.createToken("invalid-username",
+				this.properties.getAdminPassword());
 	}
-	
+
 	@Test(expected = PCMException.class)
 	public void testCreateInvalidTokenByPassword() throws PCMException {
-		this.authenticationService.createToken(this.username, "invalid-password");
+		this.authenticationService.createToken(this.properties.getAdminUsername(),
+				"invalid-password");
 	}
-	
+
 	@Test
 	public void testIsTokenValidWithValidToken() throws PCMException {
-		String accessId = this.authenticationService.createToken(this.username, this.password);
-		
+		String accessId = this.authenticationService.createToken(this.properties.getAdminUsername(),
+				this.properties.getAdminPassword());
+
 		Assert.assertTrue(this.authenticationService.isTokenValid(accessId));
 	}
-	
+
 	@Test
 	public void testIsTokenValidWithInvalidToken() throws PCMException {
-		this.authenticationService.createToken(this.username, this.password);
-		
+		this.authenticationService.createToken(this.properties.getAdminUsername(),
+				this.properties.getAdminPassword());
+
 		Assert.assertFalse(this.authenticationService.isTokenValid("fake-accessId"));
 	}
-	
+
 	@Test
 	public void testIsTokenValidWithTokenExpired() {
 		Token token = Mockito.spy(new Token());
 		Mockito.doReturn(false).when(token).isValid();
-		
+
 		this.authenticationService.addToken(token);
-		
+
 		String accessId = token.getId();
-		
+
 		Assert.assertFalse(this.authenticationService.isTokenValid(accessId));
 	}
-*/
 }
